4bf3bd4b5ef3759c11becdae8dca00804cc352e3
Jacob Lewallen <jlewallen@gmail.com>
2019-02-19 07:26:37 -0800

    Use separate spotify library.

--- main.go
+++ main.go
@@ -5,77 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/zmb3/spotify"
 )
 
-func getPlaylistByTitle(spotifyClient *spotify.Client, name string) (*spotify.SimplePlaylist, error) {
-	limit := 20
-	offset := 0
-	options := spotify.Options{Limit: &limit, Offset: &offset}
-	for {
-		playlists, err := spotifyClient.GetPlaylistsForUserOpt("jlewalle", &options)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, iter := range playlists.Playlists {
-			if strings.EqualFold(iter.Name, name) {
-				return &iter, nil
-			}
-		}
-
-		if len(playlists.Playlists) < *options.Limit {
-			break
-		}
-
-		offset := *options.Limit + *options.Offset
-		options.Offset = &offset
-	}
-
-	return nil, nil
-}
-
-func getPlaylistTracks(spotifyClient *spotify.Client, userId string, id spotify.ID) (allTracks []spotify.PlaylistTrack, err error) {
-	limit := 100
-	offset := 0
-	options := spotify.Options{Limit: &limit, Offset: &offset}
-	for {
-		tracks, spotifyErr := spotifyClient.GetPlaylistTracksOpt(userId, id, &options, "")
-		if spotifyErr != nil {
-			err = spotifyErr
-			return
-		}
-
-		allTracks = append(allTracks, tracks.Tracks...)
-
-		if len(tracks.Tracks) < *options.Limit {
-			break
-		}
-
-		offset := *options.Limit + *options.Offset
-		options.Offset = &offset
-	}
-
-	return
-}
-
 func copyPlaylist(spotifyClient *spotify.Client, userId, sourceName, destinationName string) error {
-	destination, err := getPlaylistByTitle(spotifyClient, destinationName)
+	destination, err := GetPlaylistByTitle(spotifyClient, userId, destinationName)
 	if err != nil {
 		return fmt.Errorf("Error getting %s: %v", destinationName, err)
 	}
 	if destination == nil {
-		created, err := spotifyClient.CreatePlaylistForUser(userId, destinationName, true)
+		created, err := spotifyClient.CreatePlaylistForUser(userId, destinationName, "", true)
 		if err != nil {
 			return fmt.Errorf("Unable to create playlist: %v", err)
 		}
 
 		log.Printf("Created destination: %v", created)
 
-		destination, err = getPlaylistByTitle(spotifyClient, destinationName)
+		destination, err = GetPlaylistByTitle(spotifyClient, userId, destinationName)
 		if err != nil {
 			return fmt.Errorf("Error getting %s: %v", destinationName, err)
 		}
@@ -83,12 +31,12 @@ func copyPlaylist(spotifyClient *spotify.Client, userId, sourceName, destination
 		log.Printf("Found destination: %v", destination)
 	}
 
-	main, err := getPlaylistByTitle(spotifyClient, sourceName)
+	main, err := GetPlaylistByTitle(spotifyClient, userId, sourceName)
 	if err != nil {
 		return fmt.Errorf("Error getting %s: %v", sourceName, err)
 	}
 
-	destinationTracks, err := getPlaylistTracks(spotifyClient, userId, destination.ID)
+	destinationTracks, err := GetPlaylistTracks(spotifyClient, destination.ID)
 	if err != nil {
 		return fmt.Errorf("Error getting %s: %v", sourceName, err)
 	}
@@ -98,7 +46,7 @@ func copyPlaylist(spotifyClient *spotify.Client, userId, sourceName, destination
 		destinationMap[track.Track.ID.String()] = true
 	}
 
-	mainTracks, err := getPlaylistTracks(spotifyClient, userId, main.ID)
+	mainTracks, err := GetPlaylistTracks(spotifyClient, main.ID)
 	if err != nil {
 		return fmt.Errorf("Error getting %s: %v", sourceName, err)
 	}
@@ -116,7 +64,7 @@ func copyPlaylist(spotifyClient *spotify.Client, userId, sourceName, destination
 
 		for i := 0; i < len(additions); i += 50 {
 			batch := additions[i:min(i+50, len(additions))]
-			_, err := spotifyClient.AddTracksToPlaylist(userId, destination.ID, batch...)
+			_, err := spotifyClient.AddTracksToPlaylist(destination.ID, batch...)
 			if err != nil {
 				return fmt.Errorf("Error adding tracks: %v", err)
 			}
@@ -166,11 +114,4 @@ func main() {
 
 		time.Sleep(time.Duration(o.Interval) * time.Second)
 	}
-}
-
-func min(a, b int) int {
-	if a <= b {
-		return a
-	}
-	return b
 }
